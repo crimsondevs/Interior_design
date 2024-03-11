f4cdb08ad26cb69b8825a4473dc746fb7898aa42
talhacrimsondevs <145537001+talhacrimsondevs@users.noreply.github.com>
2024-03-11 17:41:54 +0500

    Update Sidebar.tsx

--- src/components/Home/Sidebar.tsx
+++ src/components/Home/Sidebar.tsx
@@ -139,7 +139,7 @@ const SideMenu: React.FC = ({ }) => {
 
   // Poll for image
   const pollForImage = (requestId) => {
-    axios.get(`https://5t5b1kzen5vqxu-4996.proxy.runpod.net//get-uploaded-image?request_id=${requestId}`, { responseType: "blob" })
+    axios.get(`https://5t5b1kzen5vqxu-4996.proxy.runpod.net/get-uploaded-image?request_id=${requestId}`, { responseType: "blob" })
       .then(async imageResponse => { // Mark this callback as async to use await inside
         if (imageResponse.status === 200) {
           const imageUrl = URL.createObjectURL(imageResponse.data);
